65aa88bee3d261c1e67b5890d7331bb6bc0b7817
Andrey Kolybelnikov <akolybelnikov@schubergphilis.com>
2022-04-02 00:45:57 +0200

    Refactor workspace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000")
         .expect("Failed to bind address.");
-    
+
     run(listener)?.await
 }
