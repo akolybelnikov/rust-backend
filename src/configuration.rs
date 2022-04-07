b856deb74eff554a6bb66f11ee9cc2097640841a
Andrey Kolybelnikov <akolybelnikov@schubergphilis.com>
2022-04-07 15:20:03 +0200

    Configuration and multistage Docker

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,8 +5,15 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
 
 #[derive(Deserialize)]
 pub struct DatabaseSettings {
@@ -19,8 +26,49 @@ pub struct DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
     let mut settings = Config::default();
-    settings.merge(File::with_name("configuration"))?;
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // Read the "default" configuration file
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    // Detect the running environment, default to 'local'.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    // Layer on the environment-specific values.
+    settings.merge(File::from(configuration_directory.join(environment.as_str())).required(true))?;
+
     settings.try_into()
+}
+
+/// The possible runtime environment
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!("{} is not a supported environment. Use either 'local' or 'production'.", other)),
+        }
+    }
 }
 
 impl DatabaseSettings {
