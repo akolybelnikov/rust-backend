d659549e7537655b8f89b4ad14e1149379c8da13
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2022-08-14 17:25:17 +0200

    Remove unused imports

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,10 +1,8 @@
 use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{web, HttpResponse};
-use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
 use unicode_segmentation::UnicodeSegmentation;
-use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct FormData {
