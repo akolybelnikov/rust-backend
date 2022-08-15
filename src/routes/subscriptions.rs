002746e140ce5c38220bfd9bd1e8f0dbf12be25d
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2022-08-16 00:53:53 +0200

    Validate payload

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,5 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
+use actix_web::cookie::time::format_description::parse;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
@@ -12,6 +13,21 @@ pub struct FormData {
     name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+    fn try_from(form_data: FormData) -> Result<Self, Self::Error> {
+        let email = SubscriberEmail::parse(form_data.email)?;
+        let name = SubscriberName::parse(form_data.name)?;
+        Ok(Self { email, name })
+    }
+}
+
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let email = SubscriberEmail::parse(form.email)?;
+    let name = SubscriberName::parse(form.name)?;
+    Ok(NewSubscriber { email, name })
+}
+
 #[tracing::instrument(
 name = "Adding a new subscriber",
 skip(form, pool),
@@ -21,13 +37,9 @@ subscriber_name = % form.name
 )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -58,7 +70,7 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
